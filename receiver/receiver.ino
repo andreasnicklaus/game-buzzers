13421901d3122151e5234d0c7641810f7e4cbaf3
Andreas Nicklaus <andreas_nicklaus@web.de>
2024-03-14 18:35:10 +0100

    Configured input and output pins

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -3,11 +3,48 @@
 
 RH_ASK driver;
 
+// Input pins
+int RESET_INPUT_PIN = 8;
+int MODE_INPUT_PIN = 9;
+
+// button states
+int resetButtonState = 0;
+int modeButtonState = 0;
+
+// Output pins
+int LED_1_PIN = 2;
+int LED_2_PIN = 3;
+int LED_3_PIN = 4;
+int LED_4_PIN = 5;
+int LED_5_PIN = 6;
+int LED_6_PIN = 7;
+int MODE_LED_A_PIN = 10;
+int MODE_LED_B_PIN = 13;
+
 void setup()
 {
     Serial.begin(9600);	// Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+    if (!driver.init()) Serial.println("init failed");
+
+    pinMode(RESET_INPUT_PIN, INPUT);
+    pinMode(MODE_INPUT_PIN, INPUT);
+
+    pinMode(LED_1_PIN, OUTPUT);
+    pinMode(LED_2_PIN, OUTPUT);
+    pinMode(LED_3_PIN, OUTPUT);
+    pinMode(LED_4_PIN, OUTPUT);
+    pinMode(LED_5_PIN, OUTPUT);
+    pinMode(LED_6_PIN, OUTPUT);
+    pinMode(MODE_LED_A_PIN, OUTPUT);
+    pinMode(MODE_LED_B_PIN, OUTPUT);
+
+    if (driver.init()) Serial.println("setup successful");
+}
+
+void printReceivedMessage(uint8_t buf[12])
+{
+  Serial.print("Message: ");
+  Serial.println((char*)buf);
 }
 
 void loop()
@@ -16,9 +53,8 @@ void loop()
     uint8_t buflen = sizeof(buf);
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
-      int i;
-      // Message with a good checksum received, dump it.
-      Serial.print("Message: ");
-      Serial.println((char*)buf);         
+      printReceivedMessage(buf);
+
+      // TODO: implement game logic based on selected game mode
     }
 }
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -3,17 +3,33 @@
 
 RH_ASK driver;
 
+//! change for every button
+int BUTTON_NUMBER = 1;
+const char *msg = "1";
+
+int BUTTON_PIN = 10;
+int RESET_PIN = 9;
+
+int LED_PIN = 2;
+
 void setup()
 {
     Serial.begin(9600);	  // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+    if (!driver.init()) Serial.println("init failed");
+
+    pinMode(BUTTON_PIN, INPUT);
+    pinMode(RESET_PIN, INPUT);
+
+    pinMode(LED_PIN, OUTPUT);
+
+    if (driver.init()) Serial.println("init successful");
 }
 
 void loop()
 {
-    const char *msg = "Hello World!";
     driver.send((uint8_t *)msg, strlen(msg));
     driver.waitPacketSent();
     delay(1000);
+
+    // TODO: implement transmission logic for failure and reset timer
 }
