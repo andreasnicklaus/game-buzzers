a1e46c9a3662cbdd7faeb617ffce33e789260a00
Andreas Nicklaus <andreas_nicklaus@web.de>
2024-06-01 18:36:47 +0200

    implemented button logic

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -4,32 +4,44 @@
 RH_ASK driver;
 
 //! change for every button
-int BUTTON_NUMBER = 1;
-const char *msg = "1";
+const char *rf_msg = "2";
+
+const boolean debugging = true;
 
 int BUTTON_PIN = 10;
-int RESET_PIN = 9;
 
-int LED_PIN = 2;
+# define VCC2 3
+# define GND2 2
 
 void setup()
 {
-    Serial.begin(9600);	  // Debugging only
-    if (!driver.init()) Serial.println("init failed");
+    if (debugging) Serial.begin(9600);
+
+    pinMode(VCC2, OUTPUT);
+    digitalWrite(VCC2, HIGH);
+    pinMode(GND2, OUTPUT);
+    digitalWrite(GND2, LOW);
 
     pinMode(BUTTON_PIN, INPUT);
-    pinMode(RESET_PIN, INPUT);
 
-    pinMode(LED_PIN, OUTPUT);
-
+    // delay(200);
     if (driver.init()) Serial.println("init successful");
+    else Serial.println("init failed");
+}
+
+void sendDataPackage() {
+  Serial.println("Sending: " + String(rf_msg));
+  driver.send((uint8_t *)rf_msg, strlen(rf_msg));
+  driver.waitPacketSent();
 }
 
 void loop()
-{
-    driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-    delay(1000);
+{    
+    int buttonState = digitalRead(BUTTON_PIN);
 
-    // TODO: implement transmission logic for failure and reset timer
+    // button is pressed
+    if (buttonState == LOW) {
+      // Serial.println("button is pressed");
+      sendDataPackage();
+    }
 }
